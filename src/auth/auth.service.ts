260cd84a6ed342f91caa771a2d2a0c9dbd8c55d2
yorranan <yorranan.costa@outlook.com>
2024-09-19 14:15:34 -0300

    created first modules

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, Post } from "@nestjs/common";
+import {User, Bookmark} from '@prisma/client'
+import { PrismaService } from "src/prisma/prisma.service";
+@Injectable({})
+export class AuthService{
+    constructor(private prisma: PrismaService) {}
+    signin(){
+        return { msg: 'I have signed up!'};
+    }
+    signup(){
+        return { msg: 'I have signed in!'};
+    }
+}
